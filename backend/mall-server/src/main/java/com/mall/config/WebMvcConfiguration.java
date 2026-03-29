e699f7440a9e1e03e17a17662e123b6081489c84
chi_hior <15881573+chi_hior@user.noreply.gitee.com>
2026-03-29 20:16:07 +0800

    fix: disable Swagger/Knife4j in production environment

--- backend/mall-server/src/main/java/com/mall/config/WebMvcConfiguration.java
+++ backend/mall-server/src/main/java/com/mall/config/WebMvcConfiguration.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
